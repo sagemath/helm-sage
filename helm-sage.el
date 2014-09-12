05763b0bcc5d7fe88ef61149ff71ce614651d7e5
Sho Takemori <stakemorii@gmail.com>
2014-09-12 22:37:28 +0900

    Renaming

--- helm-sage.el
+++ helm-sage.el
@@ -6,7 +6,7 @@
 ;; Keywords: Sage, math, helm
 ;; Created: 2012
 ;; Version: 0.0.1
-;; Package-Requires: ((helm "20140901.59") (sage-shell "0.0.1"))
+;; Package-Requires: ((helm "20140901.59") (sage-shell-mode "0.0.1"))
 
 ;;; License
 ;; This program is free software; you can redistribute it and/or modify
@@ -37,7 +37,7 @@
 (eval-when-compile (require 'cl))
 (require 'helm)
 (require 'helm-match-plugin)
-(require 'sage-shell)
+(require 'sage-shell-mode)
 
 (defvar helm-sage-action-alist
   '(("Insert" . helm-sage-objcts-insert-action)
